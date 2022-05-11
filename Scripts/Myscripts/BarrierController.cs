48b997d0f392eac27dc856866678795bac8767e4
vanlinh00 <vanlinh20192019@gmail.com>
2022-05-11 10:06:36 +0700

    add BarrierController

---
+++ Scripts/Myscripts/BarrierController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrierController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    GameObject KnifeFractured;
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag=="Player")
+        {
+            KnifeFractured.active = true;
+            other.gameObject.active = false;
+            for(int i=0;i< KnifeFractured.transform.childCount;i++ )
+
+            {  
+                if(KnifeFractured.transform.GetChild(i).GetComponent<Rigidbody>())
+                {
+                    KnifeFractured.transform.GetChild(i).GetComponent<Rigidbody>().isKinematic = false;
+                }    
+          
+            }    
+        }    
+    }
+}
